2e9b0217c422c3a9e24dd4ee990a661d613dcf32
Denys Samoilenko <denniksam@gmail.com>
2023-03-29 11:54:30 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ASP_201.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_201.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Intro()
+        {
+            return View();
+        }
+        public IActionResult Scheme() 
+        {
+            ViewBag.bagdata = "Data in ViewBag";     // Способи передачі даних
+            ViewData["data"] = "Data in ViewData";   // до представлення
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
